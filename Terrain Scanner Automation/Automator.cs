ffe06525a8b9aeb07fc3be7e4c53a33dcdfbc8ff
Dan Koch <brakzama@gmail.com>
2017-06-07 10:31:15 -0500

    Switch from PostMessage to InputSimulator

--- Terrain Scanner Automation/Automator.cs
+++ Terrain Scanner Automation/Automator.cs
@@ -7,29 +7,12 @@ using System.Diagnostics;
 using System.Threading;
 using System.Runtime.InteropServices;
 using WindowsInput;
+using WindowsInput.Native;
 
 namespace Terrain_Scanner_Automation
 {
     public class Automator
     {
-        const UInt32 WM_KEYDOWN = 0x0100;
-        const UInt32 WM_KEYUP = 0x0101;
-        const int VK_W = 0x57;
-        const int VK_A = 0x41;
-        const int VK_D = 0x44;
-        const int VK_0 = 0x30;
-        const int VK_1 = 0x31;
-        const int VK_2 = 0x32;
-        const int VK_3 = 0x33;
-        const int VK_4 = 0x34;
-        const int VK_5 = 0x35;
-        const int VK_6 = 0x36;
-        const int VK_7 = 0x37;
-        const int VK_8 = 0x38;
-        const int VK_9 = 0x39;
-
-        [DllImport("user32.dll")]
-        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
 
         private int _minecraftProcessID;
         public int MinecraftProcessID
@@ -80,30 +63,28 @@ namespace Terrain_Scanner_Automation
             while (_running)
             {
                 MoveRightOneBlock();
-                //Thread.Sleep(500);
-                //MoveLeftOneBlock();
+                Thread.Sleep(500);
+                MoveLeftOneBlock();
             }
         }
         
         private void MoveRightOneBlock()
         {
-            
+            _input.Keyboard.KeyDown(VirtualKeyCode.VK_D);
             Thread.Sleep(100);
-            PostMessage(_minecraftProcess.MainWindowHandle, WM_KEYUP, VK_D, 0);
+            _input.Keyboard.KeyUp(VirtualKeyCode.VK_D);
         }
         
         private void MoveLeftOneBlock()
         {
-            PostMessage(_minecraftProcess.MainWindowHandle, WM_KEYDOWN, VK_A, 0);
-            Thread.Sleep(500);
-            PostMessage(_minecraftProcess.MainWindowHandle, WM_KEYUP, VK_A, 0);
+            _input.Keyboard.KeyDown(VirtualKeyCode.VK_A);
+            Thread.Sleep(100);
+            _input.Keyboard.KeyUp(VirtualKeyCode.VK_A);
         }
 
         private void TypeCoordinates()
         {
-            PostMessage(_minecraftProcess.MainWindowHandle, WM_KEYDOWN, VK_D, 0);
-            Thread.Sleep(500);
-            PostMessage(_minecraftProcess.MainWindowHandle, WM_KEYUP, VK_D, 0);
+            
         }
         
         
